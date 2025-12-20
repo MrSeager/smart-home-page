a49b194faf1403738266e8f2fdc34079f8506979
MrSeager <seager1993@gmail.com>
2025-12-20 11:23:44 +0200

    12/20/2025

--- components/MainSectionTwo.tsx
+++ components/MainSectionTwo.tsx
@@ -1,34 +1,31 @@
-//Componets
-import Image from "next/image";
+//Components
+import MainSectionTwoItem from "./MainSectionTwoItem";
 
 export default function MainSectionTwo () {
     return (
-        <div className="flex flex-col items-center gap-5 text-[#313131]">
+        <div className="flex flex-col items-center text-[#313131]">
             <h1 className="text-center font-bold text-[75px]">About us</h1>
-            <div className="grid grid-cols-2">
-                <div className="">
-                    <Image 
-                        src='/images/874bce4b3796c0c83c52001b54afa8d36f050e0b.png'
-                        alt="img"
-                        width={4096}
-                        height={2662} 
-                        className="mx-auto"
-                    />
-                </div>
-                <div className="flex flex-col items-start gap-5 justify-center lg:pe-[25%]">
-                    <h2 className="font-semibold text-base/12 text-[50px]">Smart Home&apos;s Smart Services</h2>
-                    <p className="text-[#575757]">
-                        Ye am depending property sweetness
-                        distrusts belonging collected. Smilling mention
-                        he in throught equally musical. Wisdom new
-                        and valley answer. Contented it so is discourse
-                        recommend. Man its upon him call mile. An 
-                        pasture he himself believe ferrars besides
-                        cottage.
-                    </p>
-                    <button type="button">Learn more</button>
-                </div>
-            </div>
+            <MainSectionTwoItem 
+                order1={'order-1'}
+                order2={'order-2'}
+                img={'/images/874bce4b3796c0c83c52001b54afa8d36f050e0b.png'}
+                title={"Smart Home's Smart Services"}
+                par={'Ye am depending property sweetness distrusts belonging collected. Smilling mention he in throught equally musical. Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.'}
+            />
+            <MainSectionTwoItem 
+                order1={'order-2'}
+                order2={'order-1'}
+                img={'/images/ec8068d8afb2fb687252bb17b5519087bbbbb905.png'}
+                title={"What app can do to your Appliences?"}
+                par={'Ye am depending property sweetness distrusts belonging collected. Smilling mention he in throught equally musical. Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.'}
+            />
+            <MainSectionTwoItem 
+                order1={'order-1'}
+                order2={'order-2'}
+                img={'/images/01de141d2e1ae28547efabaae468278171cff4fc.png'}
+                title={"Control Electric Appliences"}
+                par={'Ye am depending property sweetness distrusts belonging collected. Smilling mention he in throught equally musical. Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.'}
+            />
         </div>
     );
 }
---
+++ components/MainSectionTwoItem.tsx
@@ -0,0 +1,46 @@
+//Components
+import Image from "next/image";
+
+interface MainSectionTwoItemProps {
+    order1: string;
+    order2: string;
+    img: string;
+    title: string;
+    par: string;
+}
+
+export default function MainSectionTwoItem({ order1, order2, img, title, par }: MainSectionTwoItemProps) {
+    return (
+        <div className="grid grid-cols-2">
+            <div className={`relative ${order1} overflow-visible
+                            before:content-[''] 
+                            before:absolute 
+                            before:bottom-2
+                            before:left-1/5 
+                            before:w-3/5 
+                            before:h-3/5 
+                            before:bg-[#1E5AFA] 
+                            before:rounded-xl 
+                            before:z-0`}>
+                <Image 
+                    src={img}
+                    alt="img"
+                    width={3000}
+                    height={2000} 
+                    className="relative z-10 mx-auto scale-190 pointer-events-none"
+                />
+            </div>
+            <div className={`flex flex-col ${order2} ms-[25%] items-start gap-5 justify-center lg:pe-[25%]`}>
+                <h2 className="font-semibold text-base/12 text-[40px]">{title}</h2>
+                <p className="text-[#575757] lg:w-90">{par}</p>
+                <button 
+                    type="button" 
+                    className="font-semibold text-[#575757] cursor-pointer border border-2 rounded px-5 py-3 duration-500
+                                hover:bg-[#000F37] hover:border-[#000F37] hover:text-white hover:shadow-xl/30"
+                >
+                    Learn more
+                </button>
+            </div>
+        </div>
+    );
+}
